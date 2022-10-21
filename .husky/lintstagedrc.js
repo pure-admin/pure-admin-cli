aa538059d0ae132a56f232baaeb334ae123fa332
lrl <742798240@qq.com>
2022-10-21 10:49:27 +0800

    fix(workflow): update

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   '*.{js,ts}': ['eslint --fix', 'prettier --write'],
-  '{!(package)*.json,.!(browserslist)*rc}': ['prettier --write--parser json'],
   'package.json': ['prettier --write'],
   '*.md': ['prettier --write']
 }
